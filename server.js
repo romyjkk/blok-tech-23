49733b22367fce02cd262b30057e1f73603e1cb5
romyjkk <98404201+romyjkk@users.noreply.github.com>
2023-05-08 13:54:32 +0200

    ESLint aan het project toegevoegd en geconfigureerd

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-const {engine} = require('express-handlebars')
-const express = require('express')
-const app = express()
+let require;
+let process;
+
+const {engine} = require('express-handlebars');
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 // 1337
 
-app.use('/public', express.static('public'))
-app.engine('handlebars', engine({ defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
-app.set('views', 'views')
+app.use('/public', express.static('public'));
+app.engine('handlebars', engine({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
 // Handlebars.registerPartial('footerPartial', '{{footer}}');
 
 app.get('/', (req, res) => {
   res.render('login', { title: 'Login'});
-})
+});
 
 app.get('/signup', (req, res) => {
   res.render('signup', { title: 'Signup'});
-})
+});
 
 app.get('/profile', (req, res) => {
   res.render('profile', { 
     title: 'Profile',
     name: 'Romy Jongkees'
   });
-})
+});
 
 app.get('/matcher', (req, res) => {
   res.render('matcher',  { title: 'MovieMatcher'});
-})
+});
 
 app.get('*', (req,res) => {
   res.render('404', { title: '404 page not found'});
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`)
-})
+  console.log(`Server running on port: ${PORT}`);
+});

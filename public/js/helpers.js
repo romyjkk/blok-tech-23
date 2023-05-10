8203f9cd566bbece159e8cf4ce2cbe1fd8f5d2a1
romyjkk <98404201+romyjkk@users.noreply.github.com>
2023-05-10 17:04:54 +0200

    Gekut met Handlebars

---
+++ public/js/helpers.js
@@ -0,0 +1,1 @@
+Handlebars.registerPartial("footerPartial", "{{footer}}");
--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-let require;
-let process;
+// let require;
+// let process;
 
-const {engine} = require('express-handlebars');
-const express = require('express');
+const { engine } = require("express-handlebars");
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 // 1337
 
-app.use('/public', express.static('public'));
-app.engine('handlebars', engine({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.set('views', 'views');
-// Handlebars.registerPartial('footerPartial', '{{footer}}');
+app.use("/public", express.static("public"));
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.get('/', (req, res) => {
-  res.render('login', { title: 'Login'});
+app.set("views", "views");
+
+// register partial
+// const handlebars = engine.create();
+// handlebars.handlebars.registerPartial("footer", "{{footer}}");
+
+app.get("/", (req, res) => {
+  res.render("login", { title: "Login" });
 });
 
-app.get('/signup', (req, res) => {
-  res.render('signup', { title: 'Signup'});
+app.get("/signup", (req, res) => {
+  res.render("signup", { title: "Signup" });
 });
 
-app.get('/profile', (req, res) => {
-  res.render('profile', { 
-    title: 'Profile',
-    name: 'Romy Jongkees'
+app.get("/profile", (req, res) => {
+  res.render("profile", {
+    title: "Profile",
+    name: "Romy Jongkees",
   });
 });
 
-app.get('/matcher', (req, res) => {
-  res.render('matcher',  { title: 'MovieMatcher'});
+app.get("/matcher", (req, res) => {
+  res.render("matcher", { title: "MovieMatcher" });
 });
 
-app.get('*', (req,res) => {
-  res.render('404', { title: '404 page not found'});
+app.get("*", (req, res) => {
+  res.render("404", { title: "404 page not found" });
 });
 
 app.listen(PORT, () => {

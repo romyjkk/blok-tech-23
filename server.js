1fae686cc1f8a6005117c440debd9a45c1ad4cb6
romyjkk <98404201+romyjkk@users.noreply.github.com>
2023-05-13 15:54:01 +0200

    ✅ MongoDB account aangemaakt, database aangemaakt, met mongoose verbinding gemaakt met de database

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const dbKey = process.env.MONGO_URI;
 // const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const { engine } = require("express-handlebars");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 // 1337
 
@@ -12,35 +12,46 @@ app.use("/public", express.static("public"));
 app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", "views");
-
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
+// connect to the database
+async function run() {
+  try {
+    await mongoose.connect(dbKey);
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+run();
+
+// routes
+
 app.get("/", (req, res) => {
   res.render("login", { title: "Login" });
+  // the root page is the login page
 });
 
 app.get("/signup", (req, res) => {
   res.render("signup", { title: "Signup" });
 });
 
-// app.post("/profile", (req, res) => {
-//   req.render("profile", {
-//     title: "Profile",
-//     name: "Romy Jongkees",
-//   });
-// });
-
 app.post("/profile", function (req, res) {
   res.send(req.body);
+  // send form data here (later want to showcase it in a nice way, example: "Welcome, Romy!", maybe "Welcome back" if the account already exists)
 });
 
 app.get("/matcher", (req, res) => {
   res.render("matcher", { title: "MovieMatcher" });
+  // will be a static page
 });
 
 app.get("*", (req, res) => {
   res.render("404", { title: "404 page not found" });
+  // 404 is an actual hbs page
 });
 
 app.listen(PORT, () => {

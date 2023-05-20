f7f7fbebf43d84707dbc2c0f8f08f6d68cac9710
romyjkk <98404201+romyjkk@users.noreply.github.com>
2023-05-20 19:58:43 +0200

    🚧 Geprobeerd om signup data naar de database te sturen, nog niet gelukt. User schema is wel aangemaakt.

--- models/User.js
+++ models/User.js
@@ -1,33 +1,49 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const bycrypt = require("bcrypt");
 
-// // create schema
+// create schema
 
-// const userSchema = new mongoose.Schema({
-//   username: String,
-//   age: Date,
-//   email: String,
-//   password: String,
-//   // username: {
-//   //   type: String,
-//   //   required: [true, "Please provide a username!"],
-//   // },
-//   // age: {
-//   //   type: Number,
-//   //   required: [true, "Please provide an age!"],
-//   // },
-//   // email: {
-//   //   type: String,
-//   //   required: [true, "Please provide an email address!"],
-//   // },
-//   // password: {
-//   //   type: String,
-//   //   required: [true, "Please provide a password!"],
-//   // },
-// });
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: [true, "Please provide your first name!"],
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Please provide a username!"],
+    trim: true,
+    unique: [true, "This username is already in use!"],
+    min: [3, "Please make sure your username is 3 or more characters long!"],
+    max: [
+      20,
+      "Please make sure your username doesn't have more than 20 characters!",
+    ],
+  },
+  dateofbirth: {
+    type: Date,
+    required: [true, "Please provide a date of birth!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please make sure you provide an email address!"],
+    trim: true,
+    unique: [true, "This e-mail address is already in use!"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please make sure to provide a password!"],
+  },
+});
 
-// module.exports = mongoose.model("User", userSchema);
+userSchema.method({
+  async authenticate(hashedPassword) {
+    return bycrypt.compare(hashedPassword, this.password);
+  },
+});
 
-// // const UserModel = mongoose.model("user", UserSchema);
-// // module.exports = UserModel;
-// // module.exports = mongoose.model("User", userModel);
+module.exports = mongoose.model("User", userSchema);
+
+// const UserModel = mongoose.model("user", UserSchema);
+// module.exports = UserModel;
+// module.exports = mongoose.model("User", userModel);
--- server.js
+++ server.js
@@ -1,75 +1,114 @@
 // setting up the variables
 
+// server
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// engine
+
+const { engine } = require("express-handlebars");
+
+// database
+
 require("dotenv").config();
 const dbKey = process.env.MONGO_URI;
 const { MongoClient, ServerApiVersion } = require("mongodb");
-// const bodyParser = require("body-parser");
-const express = require("express");
-const app = express();
-const { engine } = require("express-handlebars");
-// const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
-// const User = require("./models/User.js");
-// 1337
+const mongoose = require("mongoose");
+
+// body parser
+const bodyParser = require("body-parser");
+
+// models
+
+const User = require("./models/User.js");
+
+// setting up handlebars and the static folder
 
 app.use("/public", express.static("public"));
 app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", "views");
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+
+// setting up body parser
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // connect to the database
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(dbKey, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+connectToDB().catch((error) => console.log(error));
 
-    // call the database and the collection
-    const db = client.db("sample_guides");
-    const coll = db.collection("comets");
-
-    const doc = {
-      orbitalPeriod: {
-        $gt: 5,
-        $lt: 85,
-      },
-    };
-
-    const result = await coll.deleteMany(doc);
-
-    // iterate over all the results
-
-    console.log("Number of documents deleted: " + result.deletedCount);
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Connected!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function connectToDB() {
+  await mongoose.connect(dbKey, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
-run().catch(console.dir);
 
 // routes
 
 app.get("/", (req, res) => {
   res.render("login", { title: "Login" });
   // the root page is the login page
+
+  // .then(item => {
+  //   res.send("Item saved to database!");
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // })
+});
+
+app.post("/", (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username, password }, (error, user) => {
+    if (error) {
+      console.error("Error logging in:", error);
+      return res.status(500).json({ error: "Failed to log in" });
+    }
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    return res.status(200).json({ message: "Logged in successfully" });
+  });
+  // ik weet nog niet zo goed wat ik hiermee ga doen
 });
 
 app.get("/signup", (req, res) => {
   res.render("signup", { title: "Signup" });
+
+  // let loginData = new User(req.body);
+  // loginData.save();
 });
+
+app.post("/signup", (req, res) => {
+  const { firstname, username, dateofbirth, email, password } = req.body;
+
+  const newUser = new User({
+    firstname,
+    username,
+    dateofbirth,
+    email,
+    password,
+  });
+
+  newUser.save();
+
+  // newUser.save((err) => {
+  //   if (err) {
+  //     console.error("Error signing up:", err);
+  //     return res.status(500).json({ error: "Failed to sign up" });
+  //   }
+  //   return res.status(200).json({ message: "Sign up successful" });
+  // });
+});
+
+// doesn't work
 
 // app.post("/profile", function (req, res) {
 //   res.send(req.body);
@@ -83,7 +122,6 @@ app.get("/matcher", (req, res) => {
 
 app.get("*", (req, res) => {
   res.render("404", { title: "404 page not found" });
-  // 404 is an actual hbs page
 });
 
 app.listen(PORT, () => {

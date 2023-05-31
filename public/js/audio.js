c10d939c538d3811a5e544f78212a11f1982ae45
romyjkk <98404201+romyjkk@users.noreply.github.com>
2023-05-31 15:02:39 +0200

    ✅ Added something to the 404 page

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,10 +1,12 @@
-// 404 page
+// 404 page (playing audio + adding filter to the image)
 
 console.log("Hello?");
 
+const lionelImage = document.querySelector(".error");
 const hello = new Audio("../public/audio/hello.mp3");
 const playPauseButton = document.querySelector(".playpausebutton");
 
 playPauseButton.addEventListener("click", () => {
   hello.play();
+  lionelImage.classList.add("filteranimation");
 });
